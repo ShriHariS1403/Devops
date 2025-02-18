17981b8294be65b4199a1eb3a8e2826e7cc1fa93
S <shrihari.s@siemens-healthineers.com>
2025-02-18 12:38:26 +0530

    changed variable name

--- calculator.cs
+++ calculator.cs
@@ -13,7 +13,7 @@ namespace calculator_c_sharp
 			string value;
 			do
 			{
-				int res;
+				int result;
 				Console.Write("Enter first number:");
 				int num1 = Convert.ToInt32(Console.ReadLine());
 				Console.Write("Enter second number:");
